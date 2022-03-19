fc25eb6e43c38ab56f4b449277016fa2e408cb2a
Narawit Rakket <43115371+TeamTamoad@users.noreply.github.com>
2022-03-19 13:15:23 +0700

    feat: initialize Card UI and scripts

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public string description;
+    public Sprite artwork;
+    public int manaCost;
+    public int attack;
+
+}
