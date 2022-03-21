bbd6c33243a48737ce464a01f4b77415f57a5079
Terash <terash2000@gmail.com>
2022-03-22 00:47:44 +0700

    update story scene

--- Assets/Scripts/Gameplay/Player/PlayerData.cs
+++ Assets/Scripts/Gameplay/Player/PlayerData.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-// All of another class that will use player data should call this static class
+// All other class that will use player data should call this static class
 public static class PlayerData
 {
     public static int maxHealth;
