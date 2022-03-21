87003b8bdc68303925a58f805e1e8f367758ce78
FinalJTth <finaljtth_31@hotmail.com>
2022-03-21 16:18:37 +0700

    Rename TopDownCharacter and properly call parent class in Player.cs

--- Assets/Scripts/Gameplay/Player/Player.cs
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MoveableSprite
 {
     protected override void Start()
     {
-
+        base.Start();
     }
 
     // Update is called once per frame
     protected override void Update()
     {
+        base.Update();
+    }
 
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(name + " Game Object Left Clicked!");
+        }
     }
 }
