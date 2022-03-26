9ac875b7761800de5343d1e93791109e720fc026
Siratish <chert31@hotmail.com>
2022-03-27 00:13:18 +0700

    fix attack for aoe target

--- Assets/Scripts/Gameplay/AttackCardContorller.cs
+++ Assets/Scripts/Gameplay/AttackCardContorller.cs
@@ -8,14 +8,19 @@ public class AttackCardContorller : CardController
     public override bool onUse(Vector3Int mousePos)
     {
         Tile tile = (Tile)Arena.singleton.tilemap.GetTile(mousePos);
-        Monster monster = MonsterManager.singleton.FindMonsterByTile(mousePos);
-        if (tile != null && monster != null && Arena.singleton.AreaPosList.Contains(mousePos))
+        if (tile == null) return false;
+        bool success = false;
+        foreach (Vector3Int pos in Arena.singleton.TargetPosList)
         {
-            Arena.singleton.hideRadius(mAreaShape, mRange);
-            monster.TakeDamage(4);
-            // monster.Stun();
-            return true;
+            Monster monster = MonsterManager.singleton.FindMonsterByTile(pos);
+            if (monster != null)
+            {
+                monster.TakeDamage(4);
+                // monster.Stun();
+                success = true;
+            }
         }
-        return false;
+        return success;
+
     }
 }
